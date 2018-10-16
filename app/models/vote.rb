ecb07009eb70c85fa384643c289c991423775113
Peter Jackson <pete@fasteragile.com>
2018-10-16 00:10:34 -0400

    Whitespace

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,5 +3,4 @@ class Vote < ApplicationRecord
   belongs_to :venue
 
   validates :value, presence: true, inclusion: {:in => [1,-1,0]}
-
 end
--- app/models/voter.rb
+++ app/models/voter.rb
@@ -1,6 +1,7 @@
 class Voter < ApplicationRecord
   has_many :votes
   has_many :venues, through: :votes
+  
   validates :first_name, presence: true
   validates :last_name, presence: true
 end
